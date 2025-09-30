b37d00f88f120559e16e7b9c41fc0d33098ee10f
Alisher <alisheryondoshaliyev77@gmail.com>
2025-09-30 17:09:30 +0500

    helpers

---
+++ helpers/db.js
@@ -0,0 +1,22 @@
+import fs from "fs/promises";
+import path from "path";
+
+const dbPath = path.join(process.cwd(), "src", "db");
+
+
+async function readData(fileName) {
+    const filePath = path.join(dbPath, fileName);
+    const data = await fs.readFile(filePath, "utf-8");
+    return JSON.parse(dbContent);
+}
+
+async function writeData(fileName, data) {
+    const filePath = path.join(dbPath, fileName);
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+
+}
+
+export const db = {
+    read: readData,
+    write: writeData
+}
